a197a634c18927cb549f10d1e2577265edf22760
ana <ana.terovic@fer.hr>
2022-10-31 00:25:42 +0100

    pretask

--- ir_exercise/test/test_pre_task.py
+++ ir_exercise/test/test_pre_task.py
@@ -5,7 +5,7 @@ class StudentIDTest(unittest.TestCase):
     # Todo:
     #  Please fill in your student id to make this test is green
     def test_student_id_filled(self):
-        student_id = ""
+        student_id = "12205927"
         self.assertNotEqual(student_id, "")
         with open("student_id.txt", "wt") as f:
             f.write(f"{student_id}")
